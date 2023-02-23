142a7b1c21577cab2741f18a8db57050f83be6e8
algaj <ales.gajdacz@gmail.com>
2023-02-23 15:43:35 +0100

    Initial structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+
+#[derive(Debug, Default)]
+struct Device6502 {
+    a: u8, /* accumulator */
+    x: u8, /* x index register */
+    y: u8, /* y index register */
+    sp: u8, /* stack pointer  */
+    pc: u8, /* program counter */
+    p: u8, /* processor flags */
+}
+
+impl Device6502 {
+    pub fn new() -> Self {
+        Self {
+            a: 0,
+            x: 0,
+            y: 0,
+            sp: 0,
+            pc: 0,
+            p: 0,
+            
+        }
+    }
+
+    pub fn tick(&mut self) {
+
+    }
+
+
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Started");
+
+    let mut device = Device6502::new();
+
+    for _ in 1..=100 {
+        device.tick();
+    }
+
+
+    println!("Finished");
 }
