cb33927337295f76142168eb9939425f88ca0229
algaj <ales.gajdacz@gmail.com>
2023-02-23 16:28:14 +0100

    IO?

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Device6502 {
+    /* Memories and registers, external state */
+    io: [bool; 40],
     a: u8, /* accumulator */
     x: u8, /* x index register */
     y: u8, /* y index register */
     sp: u8, /* stack pointer  */
     pc: u8, /* program counter */
     p: u8, /* processor flags */
+
+    /* Helpers */
+    last_clk_pin_state: bool,
 }
 
 impl Device6502 {
@@ -18,16 +22,29 @@ impl Device6502 {
             sp: 0,
             pc: 0,
             p: 0,
-            
+            io: [false; 40],
+            last_clk_pin_state: false,
         }
     }
 
     pub fn tick(&mut self) {
+        /* Update state on positive clock edge */
 
+        if (self.io[37] != self.last_clk_pin_state) {
+            /* Clock tick here */
+        }
+
+        self.last_clk_pin_state = self.io[37];
+    }
+
+    pub fn set_io(&mut self, index: usize, value: bool) {
+        self.io[index] = value;
     }
 
 
 }
+
+
 
 fn main() {
 
